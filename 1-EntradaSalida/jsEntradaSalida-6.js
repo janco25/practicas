52dbd0c85bdf0e050ee7b34ebe22b36949d4d84b
janco25 <rodolfo_puma@outlook.com>
2017-02-02 09:46:02 -0300

    2 datos
    suma de 2 datos

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,9 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+	var n,n2;
+	n = parseInt(document.getElementById('nuno').value);
+	n2 = parseInt(document.getElementById('ndos').value);
+	alert(n + n2);
 }
 
