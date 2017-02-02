23a43fd1bf7e2b622badc6198c769cbe60c1c6b5
janco25 <rodolfo_puma@outlook.com>
2017-02-02 09:34:44 -0300

    resuelto
    mostrar dos id

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -1,7 +1,9 @@
 /*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
-{	
-	
+{	var n,ed;
+	n = document.getElementById('nombre').value;
+	ed = document.getElementById('edad').value;
+	alert("nombre " + n +" edad " + ed);
 }
 
