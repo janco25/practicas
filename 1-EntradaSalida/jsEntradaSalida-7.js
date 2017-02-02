bf01c257733c5d2f6d4a8d9b9fc92cbdb3364c8c
janco25 <rodolfo_puma@outlook.com>
2017-02-02 11:06:47 -0300

    operadores

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,23 +2,34 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+var n,n2;
+	
+
 function sumar()
 {	
-	
+	n = parseInt(document.getElementById('nuno').value);
+	n2 = parseInt(document.getElementById('ndos').value);
+	alert(n + n2);
 }
 
 function restar()
 {
-	
+	n = parseInt(document.getElementById('nuno').value);
+	n2 = parseInt(document.getElementById('ndos').value);
+	alert(n - n2);
 }
 
 function multiplicar()
 { 
-	
+	n = parseInt(document.getElementById('nuno').value);
+	n2 = parseInt(document.getElementById('ndos').value);
+	alert(n * n2);
 }
 
 function dividir()
 {
-	
+	n = parseInt(document.getElementById('nuno').value);
+	n2 = parseInt(document.getElementById('ndos').value);
+	alert(n / n2);
 }
 
