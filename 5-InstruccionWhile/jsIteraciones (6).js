508b9d704e6e73ec43746c023c520684d5200855
janco25 <rodolfo_puma@outlook.com>
2017-02-15 11:13:24 -0300

    acumulador

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,10 +1,14 @@
 function Mostrar()
 {
-
+    var n;
 	var contador=0;
 	var acumulador=0;
-
-
+    while(contador<5){
+        n= parseInt(prompt("ingrese numero"));
+        acumulador=acumulador+n;
+        contador++;
+    }
+   
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
