49498a5b3fed02b7715b1f98bb2e7396f97f6f38
janco25 <rodolfo_puma@outlook.com>
2017-02-15 11:50:16 -0300

    respuesta si

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,12 +1,18 @@
 function Mostrar()
 {
 
-	var contador=0;
+	var contador=0,n;
 	var acumulador=0;
 	var respuesta='si';
-
+    
+    while(respuesta=="si"){
+        n= parseInt(prompt("ingrese numero"));
+        acumulador=acumulador+n;
+        contador++;
+        respuesta=prompt("ingresar si o no");
+    }
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+document.getElementById('promedio').value=Math.trunc(acumulador/contador);
 
 }//FIN DE LA FUNCIÓN
