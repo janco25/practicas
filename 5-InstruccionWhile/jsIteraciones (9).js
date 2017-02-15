ac0a0560acf2cb55431ff3a1c2c36ade78a85017
janco25 <rodolfo_puma@outlook.com>
2017-02-15 12:14:29 -0300

    mayor y menor

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,24 @@
 function Mostrar()
 {
 
-	var contador=0;
+	var contador=0,max,min,n;
 	// declarar variables
-	
-	var respuesta='si';
+	max=0;
+    min=15248;
+	var respuesta=true
 
-	while(respuesta!='no')
+	while(respuesta==true)
 	{
-		
-	
+        n=parseInt(prompt("ingres numero"));
+        if (max<n){
+            max=n;
+        }
+        if(min>n){
+            min=n;   
+        }
+	   respuesta=confirm("continuara ");
 	}
-
-
-
+    document.getElementById('maximo').value=max;
+    document.getElementById('minimo').value=min;
 
 }//FIN DE LA FUNCIÓN
