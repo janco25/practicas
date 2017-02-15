5276ca7219ef75d20acf29d7f1fef65913f2675d
janco25 <rodolfo_puma@outlook.com>
2017-02-15 09:53:05 -0300

    control de claves

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,23 @@
 function Mostrar()
 {
-
+var i=0;
 var clave = prompt("ingrese el número clave.");
-
-
+while(i<3){
+       if(clave=="utn750"){
+           alert("correcto");
+           i=4;
+          // break;
+       }else{
+           alert("vuelva a intertarlo: "+i);
+                      
+             clave = prompt("ingrese el número clave.")
+             if(i==2){
+                 alert("se acabaron los intentos");
+                 i=4;
+             }
+           i++;
+       }
+   
+}//fin de while
+    
 }//FIN DE LA FUNCIÓN
