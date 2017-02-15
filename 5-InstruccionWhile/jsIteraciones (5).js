8f4e2722bf12b68f83a2c90771df2713e0c94390
janco25 <rodolfo_puma@outlook.com>
2017-02-15 10:58:17 -0300

    validar char

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,10 +1,10 @@
 function Mostrar()
 {
 
-var sexo = prompt("ingrese f ó m .");
-
-
-
-document.getElementById('Sexo').value=sexo;
+    var sexo = prompt("ingrese f ó m .");
+    while(!(sexo=="m" || sexo=="f")){
+        sexo = prompt("error!!!!!!   ingrese f ó m .");
+    }
+    document.getElementById('Sexo').value=sexo;
 
 }//FIN DE LA FUNCIÓN
