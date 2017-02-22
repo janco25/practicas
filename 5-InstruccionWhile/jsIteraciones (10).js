e6fa497ea33d781cb7ac62fa010e9d340cff3e2a
janco25 <rodolfo_puma@outlook.com>
2017-02-22 09:12:29 -0300

    controles de numeros

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,47 @@
 function Mostrar()
 {
-
-	var contador=0;
+	var n,sposi=0,sneg=0,cantpos=0,cantneg=0,
+        cantcero=0,cantpar=0,promposi,promneg,diferencia;
 	//declarar contadores y variables 
 	
-	var respuesta="si";
+	var respuesta=true;
 
-	while(respuesta!="no")
+	while(respuesta!=false)
 	{
-		
-	
-	}
-
-
+        n=parseInt(prompt("ingrese un numero"));
+        if(!(n<-1))
+        {
+            sposi+=n;
+            cantpos++;
+            if (n%2==0)
+                {
+                     cantpar++;
+                }//fin if 1
+            if(n==0)
+                {
+                    cantcero++;
+                    cantpos--;
+                }//fin de if 2
+        }else
+        {
+            sneg+=n;
+            cantneg+=1;
+        }//fin de if
+            	
+	   respuesta=confirm("continuara");
+	}//fin de while
+    promposi=sposi/cantpos;
+    promneg=sneg/cantneg;
+    diferencia=sposi-sneg;
+    document.write("suma de +: "+sposi+"<br>");
+    document.write("suma de negativos: "+sneg+"<br>");
+    document.write("cant positivos: "+cantpos+"<br>");
+    document.write("cant negativos: "+cantneg+"<br>");
+    document.write("cant ceros: "+cantcero+"<br>");
+    document.write("cant pares: "+cantpar+"<br>");
+     document.write("promedio positivos: "+promposi+"<br>");
+     document.write("promedio negativos: "+promneg+"<br>");
+     document.write("diferencia en tre -+: "+diferencia);
 
 
 }//FIN DE LA FUNCIÓN
